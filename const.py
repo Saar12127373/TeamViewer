5a434d0ef1e352bcf2e727172db8cd64f3899555
Saar12127373 <saar7373@gmail.com>
2026-02-13 16:45:45 +0200

    'autosave'

---
+++ const.py
@@ -0,0 +1,12 @@
+# Configuration
+SERVER_BIND_HOST = ""
+TARGET_HOST = "192.168.1.128"
+TCP_PORT = 8090
+UDP_PORT = 8091
+
+# UI/UX
+WIN_NAME = "Remote Desktop - Live Stream"
+
+# Screen Grid (Must match on both sides)
+ROWS = 8
+COLS = 16
