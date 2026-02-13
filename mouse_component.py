fc2bbad23d9a8954dc23433d7697b73beb7f0e80
Saar12127373 <saar7373@gmail.com>
2026-02-13 17:07:17 +0200

    'autosave'

--- mouse_component.py
+++ mouse_component.py
@@ -1,7 +1,6 @@
 from pynput import mouse
 import pyautogui
 import win32api
-import struct
 from protocol import recv_all, pack_coords, unpack_coords
 
 class MouseSender:
@@ -10,10 +9,15 @@ class MouseSender:
         self.stop_event = stop_event
 
     def on_move(self, x, y):
+        # We use a non-blocking check to see if we should stop
+        if self.stop_event.is_set():
+            return False 
         self.sock.sendall(b"0")
         self.sock.sendall(pack_coords(x, y))
 
     def on_click(self, x, y, button, pressed):
+        if self.stop_event.is_set():
+            return False
         self.sock.sendall(b"1" if pressed else b"2")
         btn_id = b"3" if button == mouse.Button.left else b"4"
         self.sock.sendall(btn_id)
@@ -21,6 +25,7 @@ class MouseSender:
 
     def start(self):
         with mouse.Listener(on_move=self.on_move, on_click=self.on_click) as listener:
+            # This keeps the listener alive until stop_event is set
             while not self.stop_event.is_set():
                 listener.join(0.1)
 
@@ -28,24 +33,37 @@ class MouseReceiver:
     def __init__(self, sock, stop_event, s_res, c_res):
         self.sock = sock
         self.stop_event = stop_event
-        self.s_res = s_res # Server res (sender)
-        self.c_res = c_res # Client res (executor)
+        self.s_res = s_res # Server resolution
+        self.c_res = c_res # Client resolution
 
     def run(self):
         while not self.stop_event.is_set():
             try:
                 action = recv_all(self.sock, 1)
-                if action == b"0": # Move
-                    x, y = unpack_coords(recv_all(self.sock, 4))
+                if not action: break # Connection closed
+                
+                if action == b"0": # Move event
+                    data = recv_all(self.sock, 4)
+                    x, y = unpack_coords(data)
+                    # Mapping logic to ensure mouse reaches corners on different screens
                     mx = int(x * (self.c_res[0] / self.s_res[0]))
                     my = int(y * (self.c_res[1] / self.s_res[1]))
                     win32api.SetCursorPos((mx, my))
-                elif action in [b"1", b"2"]: # Click/Release
+                    
+                elif action in [b"1", b"2"]: # Click/Release event
                     btn_code = recv_all(self.sock, 1)
-                    x, y = unpack_coords(recv_all(self.sock, 4))
+                    data = recv_all(self.sock, 4)
+                    x, y = unpack_coords(data)
+                    
+                    # Map coordinates for clicks too so they hit the right button
+                    mx = int(x * (self.c_res[0] / self.s_res[0]))
+                    my = int(y * (self.c_res[1] / self.s_res[1]))
+                    
                     btn = "left" if btn_code == b"3" else "right"
-                    # Note: Using your original logic where x,y aren't re-mapped for clicks
-                    # but are received from the buffer.
-                    if action == b"1": pyautogui.mouseDown(button=btn)
-                    else: pyautogui.mouseUp(button=btn)
-            except: break
+                    if action == b"1": 
+                        pyautogui.mouseDown(x=mx, y=my, button=btn)
+                    else: 
+                        pyautogui.mouseUp(x=mx, y=my, button=btn)
+            except Exception as e:
+                print(f"Mouse Error: {e}")
+                break
